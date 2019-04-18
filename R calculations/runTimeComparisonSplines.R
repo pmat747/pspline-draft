3141a09953f0efba693ec334417d5853179b6a6d
Patricio Maturana Russel <33706128+pmat747@users.noreply.github.com>
2019-04-18 17:13:54 +1200

    format change

--- R calculations/runTimeComparisonSplines.R
+++ R calculations/runTimeComparisonSplines.R
@@ -21,6 +21,8 @@ x
 ### P-spline time estimates ###
 ###############################
 
+# Equidistant knots
+
 y = matrix( c(30.872, 34.873, 42.738,
               5.506, 8.185, 10.338, 
               5.490, 8.208, 10.300,
@@ -43,5 +45,32 @@ y[5,]/y[7,]; # B-splines / Psplines d=2
 y[5,]/y[8,]; # B-splines / Psplines d=3
 
 round(y[5,]/apply(y[6:8,],2,mean),3)
+
+# Distributed knots
+
+y = matrix( c(30.872, 34.873, 42.738,
+              5.629, 8.291, 10.507,
+              5.650, 8.374, 10.512,
+              5.587, 8.301, 10.427,
+              31.663, 37.066, 47.604,  
+              5.621, 8.240, 10.462,
+              5.641, 8.372, 10.549,
+              5.611, 8.327, 10.686), ncol = 3, byrow = TRUE) 
+
+
+# AR(1)
+y[1,]/y[2,]; # B-splines / Psplines d=1
+y[1,]/y[3,]; # B-splines / Psplines d=2
+y[1,]/y[4,]; # B-splines / Psplines d=3
+
+round(y[1,]/apply(y[2:4,],2,mean),3)
+
+# AR(4)
+y[5,]/y[6,]; # B-splines / Psplines d=1
+y[5,]/y[7,]; # B-splines / Psplines d=2
+y[5,]/y[8,]; # B-splines / Psplines d=3
+
+round(y[5,]/apply(y[6:8,],2,mean),3)
+
 
 
